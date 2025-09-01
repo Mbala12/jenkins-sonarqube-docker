4a4aeb42e14bbfdd9bfb22b81c00ff0473696422
Mbala12 <isaacnkongolo64@gmail.com>
2025-09-01 12:23:25 -0400

    Added New File

--- installations/install-jenkins.sh
+++ installations/install-jenkins.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y openjdk-11-jdk
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install -y jenkins
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
 
